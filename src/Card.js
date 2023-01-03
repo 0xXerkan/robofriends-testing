2bc489f8f9226c34694286696d3dd63e20f912ec
Ricky Landry <0xXerkan@gmail.com>
2023-01-03 11:40:32 -0500

    Configure React app for deployment to GitHub Pages

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = ({name, email, id}) => {
+	// Can destructure props directly when passing in
+
+	// const {name, email, id} = props;
+	return (
+		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img alt='robot' src={`https://robohash.org/${id}?size=200x200`} />
+			<div>
+				<h3>{name}</h3>
+				<p>{email}</p>
+			</div>
+		</div>
+	);
+}
+
+export default Card;
